606525fddb0ee8052daf930a60ef012b1508994b
sapark <sapark@eceutil1.gpu.snuhpc>
2024-11-06 14:33:40 +0900

    demo.sh

---
+++ demo.sh
@@ -0,0 +1,5 @@
+#messytable wh train foot
+CUDA_VISIBLE_DEVICES=0,1 python -m pdb  main.py -d messytable --train_ratio .5026 --var_extrinsic_matrices --epochs 80 --wh_train foot
+
+#messytable wh train center
+CUDA_VISIBLE_DEVICES=0,1 python -m pdb  main.py -d messytable --train_ratio .5026 --var_extrinsic_matrices --epochs 80 --wh_train center
